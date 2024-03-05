45519741c46fb62da34123afadad4b73b077a700
yuweilong <592631053@qq.com>
2024-03-05 17:42:12 +0800

    dev

--- src/common/config.js
+++ src/common/config.js
@@ -1,11 +1,10 @@
 export default {
     // url: 'https://api.test.uniai.cas-ll.cn',
-    url: 'https://api.uniai.cas-ll.cn',
+    // url: 'https://api.uniai.cas-ll.cn',
     
-    // url: 'http://192.168.98.84:3300',
+    url: 'https://api.lechat.cas-ll.cn',
     // url: 'http://192.168.41.45:3300',
     // url: 'http://192.168.41.52:3300',
     // url: 'http://192.168.3.123:3300',  
     version: '2.2.1'
   }
-  
