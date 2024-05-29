2dca22c4ebe3bea905fb84ece727962431d0a755
devilyouwei <devilyouwei@gmail.com>
2024-05-29 14:40:45 +0800

    fix echarts

--- src/common/config.js
+++ src/common/config.js
@@ -3,10 +3,10 @@
 export default {
     // url: 'https://api.test.uniai.cas-ll.cn',
     url: 'https://api.uniai.cas-ll.cn',
-    upload: 'https://api.uniai.cas-ll.cn',
+    upload: 'https://api.uniai.cas-ll.cn'
     // upload: 'http://112.80.47.186:8330',
     // url: 'http://192.168.41.52:3300',
     // url: 'http://192.168.3.123:3300',
     // url: 'http://localhost:3300',
-    version: '2.3.3'
+    // upload: 'http://localhost:3300',
 }
